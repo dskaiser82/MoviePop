a618dbc5608b1837bbcf411ba0aea462899759e7
Dan Kaiser <dskaiser82@gmail.com>
2016-02-01 15:52:25 -0800

    Added Jurassic Park

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 console.log("Hi Dan")
+
+var game = {
+        player1: {},
+        player2:{},
+        movies:[
+               {
+                 title: "Jurassic Park",
+                 director:"Steven Spielberg",
+                 writer:"David Koepp",
+                 gross:"$1.3 Billion",
+                 release:"1993",
+                 composer:"John Williams",
+                 actor:["Dr. Alan Grant","Dr. Ellie Sattler","Dr. Ian Malcolm","John Hammond","Ray Arnold","Dennis Nedry",
+                        "Henry Wu"],
+                 
+        }
+        ]
+
+
+
+
+
+
+
+
+
+
+
+}
