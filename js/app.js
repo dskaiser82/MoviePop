2b835498a503404aa26786257d4266b732b17a2a
Dan Kaiser <dskaiser82@gmail.com>
2016-02-02 11:34:40 -0800

    Added current New way to sort data and populate board

--- js/app.js
+++ js/app.js
@@ -1,29 +1,49 @@
 console.log("Hi Dan")
-
+var correctAnswer = null
 var game = {
         player1: {},
         player2:{},
+        round: 0,
+        currentQuestion: 0,
+        questions: [
+          'Who directed this movie?', // director
+          'Who wrote this movie?', // writer
+          'What did this movie gross?', // gross
+          'What was the production cost?', // production cost
+          'When was it released?', // release date
+          'Who composed the soundtrack?' // composer
+        ],
         movies: [
                {
                  title: "Jurassic Park",
-                 director:"Steven Spielberg",
-                   wrongDirectors:["James Cameron","Ed Zwick","Colin Trevorrow","Chris Columbus"],
-                 writer:"David Koepp",
-                   wrongActors:["James Cameron","Tony Gilroy","David S. Goyer"," Andy Wachowski"],
-                 gross:"$1.3 Billion",
-                   wrongGross:["$1.8 Billion","$550 Million","$257 Million"," $100 Million"],
-                 production: "$63 million",
-                   wrongProduction:["$125 Million","$55 Million","$25 Million"," $104 Million"],
-                 release:"1993",
-                   wrongRelease:["1995","1989","1997"," 2001"],
-                 composer:"John Williams",
-                    wrongcomposer:["James Horner","Hans Zimmer","Alan Silvestri","Thomas Newman"],
 
-                 actor:["Dr. Alan Grant","Dr. Ellie Sattler","Dr. Ian Malcolm","John Hammond","Ray Arnold","Dennis Nedry",
-                        "Henry Wu"],
-                 quotes: ["God creates dinosaurs. God destroys dinosaurs. God creates man. Man destroys God. Man creates dinosaurs.",
-                          "Find Nedry! Check the vending machines!","It's a UNIX system! I know this!","We have all the problems of a major theme park and a major zoo and the computer's not even on its feet yet.",
-                           "Keep absolutely still. Its vision is based on movement."],
+                 buttonOptions: [
+                   ["Steven Spielberg", "James Cameron","Ed Zwick","Colin Trevorrow","Chris Columbus"],
+                   ["David Koepp", "James Cameron","Tony Gilroy","David S. Goyer"," Andy Wachowski"],
+                   ["$1.3 Billion", "$1.8 Billion","$550 Million","$257 Million"," $100 Million"],
+                   ["$63 million", "$125 Million","$55 Million","$25 Million"," $104 Million"],
+                   ["1993", "1995","1989","1997"," 2001"],
+                   ["John Williams", "James Horner","Hans Zimmer","Alan Silvestri","Thomas Newman"]
+                 ]
+
+                //  director:"Steven Spielberg",
+                //    wrongDirectors:["James Cameron","Ed Zwick","Colin Trevorrow","Chris Columbus"],
+                //  writer:"David Koepp",
+                //    wrongActors:["James Cameron","Tony Gilroy","David S. Goyer"," Andy Wachowski"],
+                //  gross:"$1.3 Billion",
+                //    wrongGross:["$1.8 Billion","$550 Million","$257 Million"," $100 Million"],
+                //  production: "$63 million",
+                //    wrongProduction:["$125 Million","$55 Million","$25 Million"," $104 Million"],
+                //  release:"1993",
+                //    wrongRelease:["1995","1989","1997"," 2001"],
+                //  composer:"John Williams",
+                //     wrongcomposer:["James Horner","Hans Zimmer","Alan Silvestri","Thomas Newman"],
+                 //
+                //  actor:["Dr. Alan Grant","Dr. Ellie Sattler","Dr. Ian Malcolm","John Hammond","Ray Arnold","Dennis Nedry",
+                //         "Henry Wu"],
+                //  quotes: ["God creates dinosaurs. God destroys dinosaurs. God creates man. Man destroys God. Man creates dinosaurs.",
+                //           "Find Nedry! Check the vending machines!","It's a UNIX system! I know this!","We have all the problems of a major theme park and a major zoo and the computer's not even on its feet yet.",
+                //            "Keep absolutely still. Its vision is based on movement."]
 
                 },
 
@@ -40,7 +60,7 @@ var game = {
                    quotes: ["In America, it's bling bling. But out here it's bling bang.",
                             "The world is falling apart and all we hear about is blowjob-gate.",
                              "T.I.A. This is Africa."],
-                
+
 
                 },
 
@@ -63,4 +83,46 @@ var game = {
 
         ]
 
+
+
+
+
 }
+//Jurassic Park 10 Qs
+ // $("#question").html("What is Kaiser of this Monkey?");
+ // $("#1").html("Boogey");
+ // $("#2").html("Boogey");
+ // $("#3").html("Boogey");
+ // $("#4").html("Boogey");
+
+function populateBoard(){
+
+  correctAnswer = game.movies[game.round].buttonOptions[game.currentQuestion][0]
+  // var optionsArray = game.movies[game.round].wrongDirectors
+  // optionsArray.splice(Math.round(Math.random() * (optionsArray.length)), 0, correctAnswer)
+  // optionsArray.splice((optionsArray.length),0,game.movies[game.round].director)
+
+  var buttonOptions = game.movies[game.round].buttonOptions[game.currentQuestion]
+
+  // add the html to the DOM
+  $("#question").html(game.questions[game.currentQuestion]);
+  $("#1").html(buttonOptions[0]);
+  $("#2").html(buttonOptions[1]);
+  $("#3").html(buttonOptions[2]);
+  $("#4").html(buttonOptions[3]);
+  $("#5").html(buttonOptions[4]);
+}
+
+populateBoard()
+
+$('.b').click(function(){
+  console.log($(this).text())
+  // check if it's the correct answer
+  if($(this).text() == correctAnswer) {
+    console.log("Correct!")
+  } else {
+    console.log("Wrong!")
+  }
+  game.currentQuestion += 1
+  populateBoard()
+})
