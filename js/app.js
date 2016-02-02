3b097fb69daa638ddbb7901a77893615d396aa90
Dan Kaiser <dskaiser82@gmail.com>
2016-02-01 17:27:20 -0800

    trying Trailer instead of poster

--- js/app.js
+++ js/app.js
@@ -3,28 +3,46 @@ console.log("Hi Dan")
 var game = {
         player1: {},
         player2:{},
-        movies:[
-               {
-                 title: "Jurassic Park",
-                 director:"Steven Spielberg",
-                 writer:"David Koepp",
-                 gross:"$1.3 Billion",
-                 release:"1993",
-                 composer:"John Williams",
-                 actor:["Dr. Alan Grant","Dr. Ellie Sattler","Dr. Ian Malcolm","John Hammond","Ray Arnold","Dennis Nedry",
-                        "Henry Wu"],
-                 
-        }
-        ]
-
-
-
-
-
-
-
-
-
-
 
 }
+
+
+var movies = [
+       {
+         title: "Jurassic Park",
+         director:"Steven Spielberg",
+         wrongDirectors:["James Cameron","Ed Zwick","Colin Trevorrow","Chris Columbus"],
+         writer:"David Koepp",
+         gross:"$1.3 Billion",
+         production: "$63 million",
+         release:"1993",
+         composer:"John Williams",
+         actor:["Dr. Alan Grant","Dr. Ellie Sattler","Dr. Ian Malcolm","John Hammond","Ray Arnold","Dennis Nedry",
+                "Henry Wu"],
+
+        },
+
+         {
+           title: "Blood Diamond",
+           director:"Edward Zwick",
+           wrongDirectors:["James Cameron","Tony Scott","Robert Zemeckis","Ridley Scott"],
+           writer:"Charles Leavitt",
+           gross:"$171 Million",
+           production: "$100 million",
+           release:"2006",
+           composer:"James Newton Howard",
+           actor:["Danny Archer","Solomon Vandy","Maddy Bowen","Dia Vandy"],
+        },
+
+        {
+          title: "The Shawshank Redemption",
+          director:"Frank Darabont",
+          wrongDirectors:["James Cameron","Christopher Nolan","Robert Zemeckis","Ridley Scott"],
+          writer:"Frank Darabont",
+          gross:"$28 Million",
+          production: "$25 million",
+          release:"1994",
+          composer:"Thomas Newman",
+          actor:["Danny Archer","Solomon Vandy","Maddy Bowen","Dia Vandy"],
+       },
+]
